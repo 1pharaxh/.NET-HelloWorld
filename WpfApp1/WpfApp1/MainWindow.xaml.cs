39db89cca0642fff37856d8b573c3249405d3261
Akarshan Mishra <pharaohsupreme@gmail.com>
2022-10-23 16:55:29 -0600

    Added events
    Added a button click event listener that prints the name of the user

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace WpfApp1
         {
             InitializeComponent();
         }
+
+        private void submit_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Hello {FirstTextBox.Text}");
+        }
     }
 }
